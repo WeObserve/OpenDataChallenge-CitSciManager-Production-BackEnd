253d45f8be4b5fc093091d9c331fba0f3cc375f4
shiva <anindyapandey@gmail.com>
2020-08-25 03:33:27 +0530

    allow cors

---
+++ src/main/java/com/sarjom/citisci/config/filters/CORSFilter.java
@@ -0,0 +1,46 @@
+package com.sarjom.citisci.config.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORSFilter implements Filter {
+    private static Logger logger = LoggerFactory.getLogger(CORSFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        logger.info("Inside doFilter");
+
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,token,tokenId");
+        httpServletResponse.setHeader("X-Content-Type-Options", "nosniff");
+        httpServletResponse.setHeader("X-Frame-Options", "DENY");
+        httpServletResponse.setHeader("X-XSS-Protection", "1; mode=block");
+        //httpServletResponse.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+        httpServletResponse.setHeader("Content-Security-Policy", "default-src 'self'");
+
+        if (httpServletRequest.getMethod().equals(HttpMethod.OPTIONS.name())) {
+            httpServletResponse.setStatus(HttpStatus.NO_CONTENT.value());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
